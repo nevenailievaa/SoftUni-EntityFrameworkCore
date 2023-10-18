2b1db51caef809d66e24758d0240425386afd33a
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-10-18 13:50:20 +0300

    Add files via upload

---
+++ 01. ADO.NET/02. Villain Names/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+
+SqlConnection connection = new SqlConnection("Server =.;Database=MinionsDB;Integrated Security = true");
+connection.Open();
+
+using (connection)
+{
+    string query = File.ReadAllText(@"../../FindVillainsCounts.sql");
+    SqlCommand command = new SqlCommand(query, connection);
+    SqlDataReader reader = command.ExecuteReader();
+
+    using (reader)
+    {
+        while (reader.Read())
+        {
+            string villainName = (string)reader["Name"];
+            int numberOfMinions = (int)reader["NumberOfMinions"];
+            Console.WriteLine($"{villainName} {numberOfMinions}");
+        }
+    }
+}
