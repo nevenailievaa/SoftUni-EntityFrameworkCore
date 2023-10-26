cbab084682b823adef4ca98f3606491f000b8a1b
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-10-26 18:09:58 +0300

    Add files via upload

---
+++ 03. Entity Framework Introduction/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SoftUni
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Install-Package Microsoft.EntityFrameworkCore.Tools –v 6.0.1
+            //Install-Package Microsoft.EntityFrameworkCore.SqlServer –v 6.0.1
+            //Install-Package Microsoft.EntityFrameworkCore.Design -v 6.0.1
+
+            //Scaffold-DbContext -Connection "Server=DESKTOP-140PNU1\SQLEXPRESS;Database=SoftuniEF;Integrated Security=True; TrustServerCertificate=True" -Provider Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models
+        }
+    }
+}
