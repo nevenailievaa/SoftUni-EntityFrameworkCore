ca762217bcd0fce3172c8764cce995b0dc2e932c
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-11-16 20:38:55 +0200

    Add files via upload

---
+++ 09. XML Processing/02. Car Dealer/Models/PartCar.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarDealer.Models
+{
+    public class PartCar
+    {
+        //Part
+        public int PartId { get; set; }
+
+        [ForeignKey(nameof(PartId))]
+        public Part Part { get; set; } = null!; 
+
+
+        //Car
+        public int CarId { get; set; }
+
+        [ForeignKey(nameof(CarId))]
+        public Car Car { get; set; } = null!; 
+    }
+}
---
+++ 09. XML Processing/02. Car Dealer/Models/Sale.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarDealer.Models
+{
+    public class Sale
+    {
+        public int Id { get; set; }
+
+        public decimal Discount { get; set; }
+
+        public int CarId { get; set; }
+
+        [ForeignKey(nameof(CarId))]
+        public Car Car { get; set; } = null!;    
+
+        public int CustomerId { get; set; }
+
+        [ForeignKey(nameof(CustomerId))]
+        public Customer Customer { get; set; } = null!; 
+    }
+}
